0a84209dd08abbeeaf63d4dee6f90f30258f2171
Priya Shukla <prishukla.sci@gmail.com>
2023-04-08 22:07:01 -0700

    two maps: OsHV-1 spp. & OsHV-1 detections

---
+++ scripts/Map_OsHV-1_Detection.R
@@ -0,0 +1,186 @@
+######### Global Map of OsHV-1 Detections ================================
+
+######### PRESENTATION-READY MAP ==============================
+library(tidyverse)
+library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
+library(maptools) ##scalebar
+library(ggsn) ##scale bar: http://oswaldosantos.github.io/ggsn/ ; 
+
+## ggmap intro: https://appsilon.com/r-ggmap/
+
+#### load data ==============
+Map <- read_csv("data/OsHV1map_08April2023.csv")
+glimpse(Map)
+
+## unique species
+unique(Map$Paper) #35
+unique(Map$Country) #19
+unique(Map$Species) #10
+
+Map %>% 
+  group_by(Country) %>%
+  filter(OsHV1_var == "OsHV-1") %>% 
+  reframe(count = n()) #14 countries
+
+Map %>% 
+  group_by(Country) %>%
+  filter(OsHV1_var != "OsHV-1") %>% 
+  reframe(count = n()) #13 countries
+
+#### Filter out M. gigas ====
+Map.gigas <- Map %>% 
+  filter(Species == "Magallana gigas")
+
+glimpse(Map.gigas)
+
+unique(Map.gigas$Paper) #32
+unique(Map.gigas$Country) #19
+unique(Map.gigas$Species) #1
+
+## OsHV-1 type
+library(ggdark)
+
+Country.Plot <-  Map.gigas %>%
+  group_by(Country, OsHV1_var) %>% 
+  reframe(count = n()) %>% 
+  ggplot(aes(x = Country, y = count, fill = OsHV1_var, group = OsHV1_var)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  dark_theme_classic()
+
+Country.Plot
+
+#### ** PPT: OsHV-1 VarType Plot ==============
+
+#### officeR directly exports the plot to your desired file into a powerpoint slide-shaped image ===== 
+library(officer)
+## initialize R object representing .pptx file. 
+Country.Plot_fig <- read_pptx()
+Country.Plot_fig <- add_slide(Country.Plot_fig , layout = "Title and Content", master = "Office Theme")
+Country.Plot_fig <-  ph_with(x = Country.Plot_fig, value = Country.Plot, location = ph_location_fullsize() )
+Country.Plot_fig  <- ph_with(x = Country.Plot_fig, "Plot", location = ph_location_type(type = "title") )
+print(Country.Plot_fig, target = "presentations/plot.pptx")
+
+#### R2PPT / RDCOMClient =====
+
+#install.packages("devtools", dependencies = TRUE)
+
+library(RDCOMClient)
+library(R2PPT)
+
+## Step 1: Save as a temporary file
+TEMP_FILE <- paste(tempfile(), ".wmf", sep="")
+ggsave(TEMP_FILE, plot = Country.Plot) # Saving the plot to the temporary file
+
+
+## Step 2: Open a blank PPT slide
+mkppt <- PPT.Init (method = "RDCOMClient")
+mkppt <- PPT.AddBlankSlide(mkppt)
+
+## Step 3: Export graph to PPT slide
+mkppt <- PPT.AddGraphicstoSlide(mkppt, file = TEMP_FILE)
+
+unlink(TEMP_FILE)
+
+######################################################
+
+
+######## Map of OsHV-1 Detections ==========================
+
+#### OsHV-1 coords ==============
+
+glimpse(Map.gigas)
+
+OsHV1_coord <- Map.gigas %>% 
+  filter(!is.na(GPS_lat),
+         !is.na(GPS_long))
+
+glimpse(OsHV1_coord)
+
+### latitudes
+Map_nonVarlat <- OsHV1_coord$GPS_lat
+Map_nonVarlat
+
+### longitudes
+Map_nonVarlong <- OsHV1_coord$GPS_long
+Map_nonVarlong
+
+nonVarSites.df <- data.frame(
+  lon = Map_nonVarlong,
+  lat = Map_nonVarlat)
+
+
+
+glimpse(nonVarSites.df)
+
+#### OsHV-1 uvar data ==============
+OsHV1Var_coord <- Map.gigas %>% 
+  filter(OsHV1_var != "OsHV-1",
+         !is.na(GPS_lat),
+         !is.na(GPS_long))
+
+OsHV1Var_coord$GPS_lat <- as.double(OsHV1Var_coord$GPS_lat)
+OsHV1Var_coord$GPS_lat
+
+### latitudes
+Map_Varlat <- OsHV1Var_coord$GPS_lat
+Map_Varlat
+
+### longitudes
+Map_Varlong <- OsHV1Var_coord$GPS_long
+Map_Varlong
+
+VarSites.df <- data.frame(
+  lon = Map_Varlong,
+  lat = Map_Varlat)
+
+glimpse(VarSites.df)
+
+## get Maps API Key
+register_google(key = "AIzaSyAPHAhoKrfamwGo3d06FAirHsuqU6dOvZM", write = TRUE) #that is my "Maps API Key": https://console.cloud.google.com/apis/credentials?project=garbage-cat 
+
+#create a data.frame
+#sites.df <- data.frame(
+#  lon = Spp_long,
+#  lat = Spp_lat)
+# glimpse(sites.df)
+
+#sites.labels <- data.frame(
+#lon = c(-122.947833, -122.927504, -122.865700),
+#lat = c(38.218050, 38.205616, 38.120200),
+#site.name = c("HI", "BB", "TB"))
+#glimpse(sites.labels)
+
+nonVarSites.df
+
+#load a googlemap 
+get_googlemap(center = "Atlantic Ocean", zoom = 1, markers = nonVarSites.df, scale = 2,  maptype = "hybrid") %>% ggmap()
+
+## generate high quality maps using geom_point() to generate markers
+
+# satellite style map of California with Zoom
+Detection_Map <- get_map("Atlantic Ocean", zoom =  1, maptype = "satellite")
+
+ggmap(Detection_Map) +
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = '#FFDB58', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = '#D53E4F', alpha = 0.7,  size = 5)
+#geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.05, nudge_y = 0.006, hjust = 1)
+
+# Tone-Lite Map
+qmap("Atlantic Ocean", zoom = 1, scale = 2, source = "stamen", maptype = "toner-lite") +
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = '#FFDB58', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = '#D53E4F', alpha = 0.7,  size = 5)
+#geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
+
+# Watercolor Map
+qmap("Atlantic Ocean", zoom = 1, scale = 2, source = "stamen", maptype = "watercolor") + ## Tomales Bay - Artistic
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = 'purple', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = 'red', alpha = 0.7,  size = 5)
+
+#geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
+
+TB_watercolor
+
+
--- scripts/Map_OsHV-1_Species.R
+++ scripts/Map_OsHV-1_Species.R
@@ -1,4 +1,4 @@
-######### Global Map with infected species locations ================================
+######### Global Map with reservoirs ================================
 
 
 ######### PRESENTATION-READY MAP ==============================
@@ -13,63 +13,222 @@ library(ggsn) ##scale bar: http://oswaldosantos.github.io/ggsn/ ;
 ## ggmap intro: https://appsilon.com/r-ggmap/
 
 #### load data ==============
-Spp <- read_csv("data/OsHV-1_Spp_23Mar23.csv")
+Spp <- read_csv("data/OsHV1reservoirs_08April2023.csv")
+glimpse(Spp)
+
+## unique species
+unique(Spp$Paper) #39
+unique(Spp$Country) #16
+unique(Spp$Species) #29
+unique(Spp$Taxa) #12
+
+
+
+#### Filter out M. gigas ====
+
+Spp.NoGigas <- Spp %>% 
+  filter(Species != "Magallana gigas",
+         !is.na(Country))
+
+colSums(is.na(Spp.NoGigas))
+
+## unique species w/o M. gigas
+unique(Spp.NoGigas$Paper) #39
+unique(Spp.NoGigas$Country) #14
+unique(Spp.NoGigas$Species) #28
+unique(Spp.NoGigas$Taxa) #9
+
+
+#### Plot Count of Other taxa ====
+library(ggdark)
+
+Reservoirs.Plot <-  Spp.NoGigas %>% 
+  group_by(Taxa) %>% 
+  summarize(count = n()) %>% 
+  ggplot(aes(x = Taxa, y = count, fill = Taxa)) +
+  geom_bar(stat = "identity") +
+  dark_theme_classic()
+
+Reservoirs.Plot
+
+#### ** PPT: Reservoirs Plot ==============
+
+#### officeR directly exports the plot to your desired file into a powerpoint slide-shaped image ===== 
+library(officer)
+## initialize R object representing .pptx file. 
+Reservoirs.Plot_fig <- read_pptx()
+Reservoirs.Plot_fig <- add_slide(Reservoirs.Plot_fig , layout = "Title and Content", master = "Office Theme")
+Reservoirs.Plot_fig <-  ph_with(x = Reservoirs.Plot_fig, value = Reservoirs.Plot, location = ph_location_fullsize() )
+Reservoirs.Plot_fig  <- ph_with(x = Reservoirs.Plot_fig, "Plot", location = ph_location_type(type = "title") )
+print(Reservoirs.Plot_fig, target = "presentations/plot.pptx")
+
+#### R2PPT / RDCOMClient =====
+
+#install.packages("devtools", dependencies = TRUE)
+
+library(RDCOMClient)
+library(R2PPT)
+
+## Step 1: Save as a temporary file
+TEMP_FILE <- paste(tempfile(), ".wmf", sep="")
+ggsave(TEMP_FILE, plot = Reservoirs.Plot) # Saving the plot to the temporary file
+
+
+## Step 2: Open a blank PPT slide
+mkppt <- PPT.Init (method = "RDCOMClient")
+mkppt <- PPT.AddBlankSlide(mkppt)
+
+## Step 3: Export graph to PPT slide
+mkppt <- PPT.AddGraphicstoSlide(mkppt, file = TEMP_FILE)
+
+unlink(TEMP_FILE)
+
+######################################################
+
+#### OsHV-1 Var Type Plot==============
+
+## "Other Oyster" species
+Spp.NoGigas %>% 
+  filter(Taxa == "Other Oyster") %>% 
+  reframe(Ostreids = unique(Species))
+
+## OsHV-1 type
+Var.Plot <-  Spp.NoGigas %>%
+  filter(Species != "Magallana gigas") %>% 
+  group_by(Taxa, OsHV1_var) %>% 
+  reframe(count = n()) %>% 
+  ggplot(aes(x = Taxa, y = count, fill = OsHV1_var, group = OsHV1_var)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  dark_theme_classic()
+
+Var.Plot
+
+#### ** PPT: OsHV-1 VarType Plot ==============
+
+#### officeR directly exports the plot to your desired file into a powerpoint slide-shaped image ===== 
+library(officer)
+## initialize R object representing .pptx file. 
+Var.Plot_fig <- read_pptx()
+Var.Plot_fig <- add_slide(Var.Plot_fig , layout = "Title and Content", master = "Office Theme")
+Var.Plot_fig <-  ph_with(x = Var.Plot_fig, value = Var.Plot, location = ph_location_fullsize() )
+Var.Plot_fig  <- ph_with(x = Var.Plot_fig, "Plot", location = ph_location_type(type = "title") )
+print(Var.Plot_fig, target = "presentations/plot.pptx")
+
+#### R2PPT / RDCOMClient =====
+
+#install.packages("devtools", dependencies = TRUE)
+
+library(RDCOMClient)
+library(R2PPT)
+
+## Step 1: Save as a temporary file
+TEMP_FILE <- paste(tempfile(), ".wmf", sep="")
+ggsave(TEMP_FILE, plot = Var.Plot) # Saving the plot to the temporary file
+
+
+## Step 2: Open a blank PPT slide
+mkppt <- PPT.Init (method = "RDCOMClient")
+mkppt <- PPT.AddBlankSlide(mkppt)
+
+## Step 3: Export graph to PPT slide
+mkppt <- PPT.AddGraphicstoSlide(mkppt, file = TEMP_FILE)
+
+unlink(TEMP_FILE)
+
+######################################################
+
+
+######## Map of OsHV-1 Reservoirs ==========================
 
 ### latitudes
-Spp_lat <- Spp$GPS_lat
+Spp_lat <- Spp.NoGigas$GPS_lat
 Spp_lat
 
 ### longitudes
-Spp_long <- Spp$GPS_long
+Spp_long <- Spp.NoGigas$GPS_long
 Spp_long
 
-## unique species
-unique(Spp$Paper) #38 as of March 23, 2023
-unique(Spp$Country) #15 as of March 23, 2023
-unique(Spp$Species) #29 as of March 23, 2023
-unique(Spp$Taxa) #12 as of March 23, 2023
+#### OsHV-1 coords ==============
 
+OsHV1_coord <- Spp.NoGigas %>% 
+  filter(OsHV1_var == "OsHV-1")
+
+OsHV1_coord
+
+### latitudes
+Spp_nonVarlat <- OsHV1_coord$GPS_lat
+Spp_nonVarlat
+
+### longitudes
+Spp_nonVarlong <- OsHV1_coord$GPS_long
+Spp_nonVarlong
+
+nonVarSites.df <- data.frame(
+  lon = Spp_nonVarlong,
+  lat = Spp_nonVarlat)
+
+glimpse(nonVarSites.df)
+
+#### OsHV-1 uvar data ==============
+OsHV1Var_coord <- Spp.NoGigas %>% 
+  filter(OsHV1_var != "OsHV-1")
+
+### latitudes
+Spp_Varlat <- OsHV1Var_coord$GPS_lat
+Spp_Varlat
+
+### longitudes
+Spp_Varlong <- OsHV1Var_coord$GPS_long
+Spp_Varlong
+
+VarSites.df <- data.frame(
+  lon = Spp_Varlong,
+  lat = Spp_Varlat)
+
+glimpse(VarSites.df)
 
 
 ## get Maps API Key
 register_google(key = "AIzaSyAromYd5yoy--uNE9ANyPyWCS1PdGZwYGg", write = TRUE) #that is my "Maps API Key": https://console.cloud.google.com/apis/credentials?project=garbage-cat 
 
-#create a data.frame with Hog Island, Bodega Bay & Tomales Bay oyster company leases
-sites.df <- data.frame(
-  lon = Spp_long,
-  lat = Spp_lat)
-glimpse(sites.df)
+#create a data.frame
+#sites.df <- data.frame(
+#  lon = Spp_long,
+#  lat = Spp_lat)
+# glimpse(sites.df)
 
-sites.labels <- data.frame(
+#sites.labels <- data.frame(
   #lon = c(-122.947833, -122.927504, -122.865700),
   #lat = c(38.218050, 38.205616, 38.120200),
   #site.name = c("HI", "BB", "TB"))
-glimpse(sites.labels)
+#glimpse(sites.labels)
 
 
 #load a googlemap 
-get_googlemap(center = "Atlantic Ocean", zoom = 1, markers = sites.df, scale = 2,  maptype = "hybrid") %>% ggmap()
+get_googlemap(center = "Atlantic Ocean", zoom = 1, markers = nonVarSites.df, scale = 2,  maptype = "hybrid") %>% ggmap()
 
 ## generate high quality maps using geom_point() to generate markers
 
 # satellite style map of California with Zoom
-Spp_Map <- get_map("TAtlantic Ocean", zoom =  1, maptype = "satellite")
+Spp_Map <- get_map("Atlantic Ocean", zoom =  1, maptype = "satellite")
 
 ggmap(Spp_Map) +
-  geom_point(data = sites.df, aes(x = lon, y = lat), color = 'purple', alpha = 0.7,  size = 5) #+
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = '#FFDB58', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = '#D53E4F', alpha = 0.7,  size = 5)
   #geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.05, nudge_y = 0.006, hjust = 1)
 
 # Tone-Lite Map
 qmap("Atlantic Ocean", zoom = 1, scale = 2, source = "stamen", maptype = "toner-lite") +
-  geom_point(data = sites.df, aes(x = lon, y = lat), color = 'black', alpha = 0.5,  size = 4) #+
-  #geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = '#FFDB58', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = '#D53E4F', alpha = 0.7,  size = 5)
+#geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
 
 # Watercolor Map
 qmap("Atlantic Ocean", zoom = 1, scale = 2, source = "stamen", maptype = "watercolor") + ## Tomales Bay - Artistic
-  geom_point(data = sites.df, aes(x = lon, y = lat), color = 'black', alpha = 0.5,  size = 4) #+
-  #geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
+  geom_point(data = nonVarSites.df, aes(x = lon, y = lat), color = 'purple', alpha = 0.7,  size = 5) +
+  geom_point(data = VarSites.df, aes(x = lon, y = lat), color = 'red', alpha = 0.7,  size = 5)
 
-TB_watercolor
+  #geom_text(data = sites.labels, aes(x = lon, y = lat, label = site.name), nudge_x = 0.015, nudge_y = 0.006, hjust = 1)
 
 
 ###########
