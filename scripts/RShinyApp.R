7b4eebff240716a587b3460bb5f7ba7e4987c9a2
Priya Shukla <prishukla.sci@gmail.com>
2023-07-13 17:48:38 -0700

    push data to the repo

---
+++ scripts/RShinyApp.R
@@ -0,0 +1,22 @@
+### Making an R shiny interactive map that overlays on top of Google Maps
+### Interactive Map: https://appsilon.com/interactive-google-maps-with-r-shiny/
+### R Shiny Dashboard w/ Applsilon: https://appsilon.com/
+### shinydashboard: https://rstudio.github.io/shinydashboard/get_started.html
+
+library(tidyverse)
+
+
+#### Install package(s)
+#install.packages(c("shiny", "shinydashboard"))
+install.packages(c("flexdashboard",
+               "highcharter",
+               "gt",
+               "htmltools",
+               "viridis"))
+
+library(tidyverse)
+library(flexdashboard)
+library(highcharter)
+library(gt)
+library(htmltools)
+library(viridis)
